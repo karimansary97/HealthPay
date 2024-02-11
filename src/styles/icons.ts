f7b0e5c5526d41d28fb632cb16fc8a5314768767
Kareem Ansary <kareem.ansary@everestminds.com>
2024-02-11 02:05:10 +0200

    add colors and icons

--- src/styles/icons.ts
+++ src/styles/icons.ts
@@ -1,3 +1,6 @@
 export {default as DeleteIcon} from '../assets/delete.svg';
 export {default as ShareIcon} from '../assets/share.svg';
 export {default as PlusIcon} from '../assets/plus.svg';
+export {default as CloseIcon} from '../assets/Close.svg';
+export {default as AddIcon} from '../assets/add.svg';
+export {default as NoTask} from '../assets/noTask.svg';
